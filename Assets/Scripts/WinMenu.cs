bd83aaec7951688819efa3afcacc874bfa2c2a2b
solene317 <74255326+solene317@users.noreply.github.com>
2024-03-28 10:15:27 -0400

    Différents menus du jeu

---
+++ Assets/Scripts/WinMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class WinMenu : MonoBehaviour
+{
+    public void ReLoadGame()
+    {
+        SceneManager.LoadScene("House");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
